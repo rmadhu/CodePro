5f6c7358629aeb2bd6d76bbe8a2ba123c9464eb5
Madhu <ramalingam.madhu@gmail.com>
2014-08-29 22:31:08 +1000

    Add new class

---
+++ src/collections/HashMapExample.java
@@ -0,0 +1,28 @@
+package collections;
+//Hashmap example
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class HashMapExample {
+	public static void main(String[] args) {
+		HashMap <String, String> books = new HashMap<String, String>();
+		
+		//Add elements
+		books.put("One hundred years of solitude", "Garcia Gabriel Marquez");
+		books.put("The space between us", "Thrity Umrigar");
+		books.put("Unaccustomed Earth","Jhumpa Lahiri");
+		books.put("Rebecca", "Daphne du Maurier");
+		books.put("Interpreter of Maladies","Jhumpa Lahiri");
+		books.put("Cutting for Stone","Abraham Verghese");
+		
+		//Display elements
+		Iterator<String> i = books.keySet().iterator();
+		System.out.println("A few favorites:");
+		while(i.hasNext()){
+			String result = (String) i.next();
+			System.out.println("Title: "+result + " *** Author: "+books.get(result));
+		}
+	}
+
+}
