73c15da0a68f581567b7d8ea87376a4bd0e2c803
Madhu <ramalingam.madhu@gmail.com>
2014-09-01 11:04:50 +1000

    modified comment

--- src/collections/SimpleLRUCache.java
+++ src/collections/SimpleLRUCache.java
@@ -4,20 +4,21 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 //A simple least recently used cache implementation using LinkedHashMap
-public class SimpleLRUCache <K, V> extends LinkedHashMap <K, V>{
+public class SimpleLRUCache<K, V> extends LinkedHashMap<K, V> {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 0;
 	private int maxSize;
+	private int timeToLive;
 
 	public SimpleLRUCache(int maxSize) {
 		super(maxSize, 0.75f, true);
 		this.maxSize = maxSize;
 	}
-	
+
 	@Override
-	protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+	protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
 		return size() > maxSize;
 	}
 
